6964b80812f87cd12b335d1dfd47d73ec3f380db
Nguyen Thanh Luan <ntluan0301@gmail.com>
2021-10-06 10:53:00 +0700

    udpate code

--- index.js
+++ index.js
@@ -3,5 +3,21 @@ const app=express()
 app.get("/",(req,res)=>{
     res.send("hello world")
 })
+app.get("/users",(req,res)=>{
+    let uses=[
+        {
+            "id":1,
+            "username":"user 1"
+        },
+        {
+            id:2,
+            username:"user2"
+        },
+        {
+            id:3,
+            username:3
+        }
+    ]
+})
 const port = process.env.PORT || 5000
 app.listen(port)
